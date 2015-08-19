5a707a6e4af90fdb2d50857a9e54a6d11103db37
Benjamin Canou <benjamin@canou.fr>
2015-08-19 11:14:37 +0200

    Some new thoughts

--- rddl.ml
+++ rddl.ml
@@ -1,4 +1,4 @@
-(* AST *)
+(** AST *)
 
 (************************************************************************)
 (*  rddl - reactive design description language                         *)
@@ -17,57 +17,118 @@
 (*                                                                      *)
 (************************************************************************)
 
-type screen =
-  { name : string ;
-    components : component list ;
-    views : view list ;
-    sources : string list }
+type ui =
+  { pages : page table ;
+    profiles : context table }
+
+and page =
+  { views : view list ;
+    components : (component * flags) table ;
+    containers : (container * flags) table ;
+    sources : source table }
 
 and view =
-  { name : string ;
-    hierarchy : hierarchy ;
-    context : context }
+  { document : element ;
+    compatible_profiles : string list }
+
+and container =
+  { extensible : bool }
 
 and component =
-  { name : string ;
-    hideable : bool ;
-    optional : bool ;
-    container : bool }
+  { ratio : float range }
 
+and flags =
+  | Required
+  | Hideable
+  | Optional
+
+and source =
+  | Uri of string
+  | Custom
+
+and 'a table =
+  (string * 'a) list
+
+(** Rules to describe the context profiles. *)
 and context =
-  (* Meta *)
   | And of context list
+  (** This rule applies if all its sub-rules apply. *)
   | Or of context list
+  (** This rule applies if any of its sub-rules apply. *)
   (* Capabilities *)
-  | Output of [ `textual | `simplified | `standard | `fancy ] range (* remove 1 ? *)
+  | Output of [ `textual | `simplified | `fancy ] range
   | Interactivity of [ `view_only | `pointer | `single_touch | `multi_touch  ]
   | Size of [ `display_width | `display_height | `device_width | `device_height ] * float range
   | Aspect_ratio of [ `device | `display ] * float range (* width / height *)
   | Resolution of float range (* dots per inch *)
   (* Accessibility *)
-  | Contrast of [ `low | `normal | `high ] range (* colorblind ? *)
+  | Contrast of [ `low | `normal | `high ] range
   | Ink of [ `low | `normal | `high ] range
   | Zoom of [ `low | `normal | `high ] range
+  (* TODO: colors *)
   (* Environment *)
   | Connected of source * bool
-  | Bandwitdh of int range (* kilobits per second *) (* virtual ? which component decides ?*)
+  | Bandwitdh of source * int range (* kilobits per second *)
 
 and 'a range =
   { min : 'a option ;
     max : 'a option }
 
-and structure =
-  { kind : component ;
-    children : structure list }
+and element =
+  | Vertical of element list * string option
+  | Horizontal of element list * string option
+  | Flow of element list * string option
+  | Menu of element list * string option
+  | Label of string * string option
+  | Custom_component of string
+  | Custom_container of element list * string
 
-and kind =
-  | Vertical_split
-  | Horizontal_split
-  | Custom of string
+(* Voir: material UI *)
 
+(*
 open Json_encoding
 
-let kind_json_value_encoding : kind value_encoding =
-  value_encoding @@ string_enum
-    [ "vertical-split", Vertical_split ;
-      "horizontal-split", Horizontal_split ]
+let element_encoding =
+  mu "element" @@ fun self ->
+  let component_encoding =
+    conv
+      (fun { name; hideable; optional } -> (name, hideable, optional))
+      (fun (name, hideable, optional) -> { name; hideable; optional }) @@
+    obj3
+      (req "name" string)
+      (req "hideable" bool)
+      (req "optional" bool) in
+  let properties_encoding =
+    conv
+      (fun { children; dynamic; component } -> (children, dynamic, component))
+      (fun (children, dynamic, component) -> { children; dynamic; component }) @@
+    obj3
+      (req "children" (list self))
+      (req "dynamic" bool)
+      (opt "component" component_encoding) in
+  let with_kind kind obj =
+    conv (fun o -> ((), o)) (fun ((), o) -> o) @@
+    merge_objs (obj1 (req "kind" (string_enum [ kind, () ]))) obj in
+  union
+    [ case
+        (with_kind "vertical" properties_encoding)
+        (function Vertical props -> Some props | _ -> None)
+        (fun props -> Vertical props) ;
+      case
+        (with_kind "horizontal" properties_encoding)
+        (function Horizontal props -> Some props | _ -> None)
+        (fun props -> Horizontal props) ;
+      case
+        (with_kind "menu" properties_encoding)
+        (function Menu props -> Some props | _ -> None)
+        (fun props -> Menu props) ;
+      case
+        (with_kind "component" component_encoding)
+        (function Component props -> Some props | _ -> None)
+        (fun props -> Component props) ]
+
+let () =
+  Json_encoding.schema element_encoding |>
+  Json_schema.to_json |>
+  Ezjsonm.to_channel ~minify:false stdout
+*)
