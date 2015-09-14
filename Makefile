c6b791a8c714505a444ff523250fa16bfffd62d0
Benjamin Canou <benjamin@canou.fr>
2015-09-14 16:42:17 +0200

    complete schema

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 
-all: rddl.cmx
+all: rddl
+
+rddl: rddl.cmx
+	ocamlfind ocamlopt -package "ocplib-json-typed,ezjsonm" $< -o $@ -linkpkg
 
 %.cmx: %.ml
 	ocamlfind ocamlopt -package "ocplib-json-typed,ezjsonm" -c $<
 
 %.cmi: %.mli
 	ocamlfind ocamlopt -package "ocplib-json-typed,ezjsonm" -c $<
+
+clean:
+	-rm -f *.cm* rddl *~ *.o
--- rddl.ml
+++ rddl.ml
@@ -61,7 +61,7 @@ and page =
 and view =
   { document : element ;
     (** The root container or component. *)
-    compatible_profiles : profile handle list
+    compatible_profiles : profile id list
     (** The profiles to which this view belongs. *) }
 
 (** Container specific attributes. *)
@@ -96,39 +96,38 @@ and source =
 
 (** A profile description. *)
 and profile =
-  { output : [ `textual | `simplified | `fancy ] range ;
-    interactivity : [ `view_only | `pointer | `single_touch | `multi_touch  ] ;
-    size : [ `display_width | `display_height | `device_width | `device_height ] * float range ;
-    aspect_ratio : [ `device | `display ] * float range (* width / height *) ;
+  { output : output_level range ;
+    interactivity : interactivity_level range ;
+    display_width : int range ;
+    device_width : int range ;
+    display_aspect_ratio : float range (* width / height *) ;
+    device_aspect_ratio : float range (* width / height *) ;
     resolution : float range (* dots per inch *) ;
-    contrast : [ `low | `normal | `high ] range ;
-    ink : [ `low | `normal | `high ] range ;
-    zoom : [ `low | `normal | `high ] range ;
-    connected : source handle list ;
-    bandwitdh : (source handle * int range) list (* kilobits per second *) }
+    contrast : three_steps_level range ;
+    ink : three_steps_level range ;
+    zoom : three_steps_level range ;
+    connected : source id list ;
+    bandwitdh : (source id * int range) list (* kilobits per second *) }
+
+and output_level = Textual | Simplified | Fancy
+and interactivity_level = View_only | Pointer | Single_touch | Multi_touch
+and three_steps_level = Low | Normal | High
 
 (** The tree type of containers and components. *)
 and element =
-  | Vertical of element list * container handle option
-  | Horizontal of element list * container handle option
-  | Flow of element list * container handle option
-  | Menu of element list * container handle option
-  | Custom_container of element list * container handle
-  | Label of string * component handle option
-  | Custom_component of component handle
+  | Vertical of element list * container id option
+  | Horizontal of element list * container id option
+  | Flow of element list * container id option
+  | Menu of element list * container id option
+  | Custom_container of element list * container id
+  | Label of string * component id option
+  | Custom_component of component id
 
+(** An identifier for a value of a given type. *)
+and 'a id = string
 
-(** An explicitly named ['a] node. *)
-and 'a handle =
-  { id : string ;
-    (** The machine readable name. *)
-    name : string ;
-    (** The human readable name / description. *)
-    resolved : 'a
-    (** The boxed value. *) }
-
-(** A set of named values. *)
-and 'a table = 'a handle list
+(** A table of identified values of a given type. *)
+and 'a table = ('a id * 'a) list
 
 (** A range of ['a]s. [{ min = None ; max = None }] means any value. *)
 and 'a range =
@@ -137,52 +136,212 @@ and 'a range =
     max : 'a option
     (** The inclusive maximum. *) }
 
-(* Voir: material UI *)
+let empty_range =
+  { min = None ; max = None }
 
-(*
 open Json_encoding
 
-let element_encoding =
-  mu "element" @@ fun self ->
-  let component_encoding =
-    conv
-      (fun { name; hideable; optional } -> (name, hideable, optional))
-      (fun (name, hideable, optional) -> { name; hideable; optional }) @@
-    obj3
-      (req "name" string)
-      (req "hideable" bool)
-      (req "optional" bool) in
-  let properties_encoding =
-    conv
-      (fun { children; dynamic; component } -> (children, dynamic, component))
-      (fun (children, dynamic, component) -> { children; dynamic; component }) @@
-    obj3
-      (req "children" (list self))
-      (req "dynamic" bool)
-      (opt "component" component_encoding) in
-  let with_kind kind obj =
-    conv (fun o -> ((), o)) (fun ((), o) -> o) @@
-    merge_objs (obj1 (req "kind" (string_enum [ kind, () ]))) obj in
+let id_encoding value_encoding = string
+
+let table_encoding value_encoding = assoc value_encoding
+
+let range_encoding ?(compare = Pervasives.compare) enc =
+  union
+    [ case enc
+        (function { min = Some v ; max = Some v' } when v = v' -> Some v | _ -> None)
+        (fun v -> { min = Some v ; max = Some v }) ;
+      case (obj2 (opt "min" enc) (opt "max" enc))
+        (fun { min ; max } -> Some (min, max))
+        (fun (min, max) -> { min ; max }) ]
+
+let source_encoding =
+  def "source" @@
   union
     [ case
-        (with_kind "vertical" properties_encoding)
-        (function Vertical props -> Some props | _ -> None)
-        (fun props -> Vertical props) ;
+        (obj1 (req "uri" string))
+        (function Uri uri -> Some uri | Custom -> None)
+        (function uri -> Uri uri) ;
       case
-        (with_kind "horizontal" properties_encoding)
-        (function Horizontal props -> Some props | _ -> None)
-        (fun props -> Horizontal props) ;
+        (string_enum [ "custom", () ])
+        (function Uri _ -> None | Custom -> Some ())
+        (function () -> Custom) ]
+
+let priority_encoding =
+  string_enum
+    [ "optional", Optional ;
+      "hideable", Hideable ;
+      "required", Required ]
+
+let container_encoding =
+  conv
+    (fun { container_extensible ; container_priority } ->
+       (container_extensible, container_priority))
+    (fun (container_extensible, container_priority) ->
+       { container_extensible ; container_priority }) @@
+  obj2
+    (dft "extensible" bool false)
+    (req "priority" priority_encoding)
+
+let component_encoding =
+  conv
+    (fun { component_aspect_ratio ; component_priority } ->
+       (component_aspect_ratio, component_priority))
+    (fun (component_aspect_ratio, component_priority) ->
+       { component_aspect_ratio ; component_priority }) @@
+  obj2
+    (dft "aspect_ratio" (range_encoding float) empty_range)
+    (req "priority" priority_encoding)
+
+let profile_encoding =
+  def "profile" @@
+  conv
+    (fun { output ; interactivity ;
+           display_width ; display_aspect_ratio ; device_width ; device_aspect_ratio ; resolution ;
+           contrast ; ink ; zoom ;
+           connected ; bandwitdh } ->
+      (((output, interactivity),
+        (display_width, display_aspect_ratio, device_width, device_aspect_ratio, resolution)),
+       ((contrast, ink, zoom),
+        (connected, bandwitdh))))
+    (fun
+      (((output, interactivity),
+        (display_width, display_aspect_ratio, device_width, device_aspect_ratio, resolution)),
+       ((contrast, ink, zoom),
+        (connected, bandwitdh))) ->
+      { output ; interactivity ;
+        display_width ; display_aspect_ratio ; device_width ; device_aspect_ratio ; resolution ;
+        contrast ; ink ; zoom ;
+        connected ; bandwitdh }) @@
+  merge_objs
+    (merge_objs
+       (obj2
+          (dft "output"
+             (range_encoding
+                (string_enum [ "textual", Textual ;
+                               "simplified", Simplified ;
+                               "fancy", Fancy ]))
+             empty_range)
+          (dft "interactivity"
+             (range_encoding
+                (string_enum [ "view only", View_only ;
+                               "pointer", Pointer ;
+                               "single touch", Single_touch ;
+                               "multi touch", Multi_touch ]))
+             empty_range))
+       (obj5
+          (dft "displayWidth" (range_encoding int) empty_range)
+          (dft "displayAspectRatio" (range_encoding float) empty_range)
+          (dft "deviceWidth" (range_encoding int) empty_range)
+          (dft "deviceAspectRatio" (range_encoding float) empty_range)
+          (dft "resolution" (range_encoding float) empty_range)))
+    (merge_objs
+       (let three_steps_level_encoding =
+          string_enum [ "low", Low ; "normal", Normal ; "high", High ] in
+        obj3
+          (dft "contrast" (range_encoding three_steps_level_encoding) empty_range)
+          (dft "ink" (range_encoding three_steps_level_encoding) empty_range)
+          (dft "zoom" (range_encoding three_steps_level_encoding) empty_range))
+       (obj2
+          (dft "connected" (list (id_encoding source_encoding)) [])
+          (dft "bandwidth" (assoc (range_encoding int)) [])))
+
+let element_encoding =
+  mu "element" @@ fun element_encoding ->
+  union
+    [ case
+        (obj3
+           (req "type" (string_enum ["vertical", ()]))
+           (req "children" (list element_encoding))
+           (opt "id" (id_encoding container_encoding)))
+        (function Vertical (children, id) -> Some ((), children, id) | _ -> None)
+        (fun ((), children, id) -> Vertical (children, id)) ;
       case
-        (with_kind "menu" properties_encoding)
-        (function Menu props -> Some props | _ -> None)
-        (fun props -> Menu props) ;
+        (obj3
+           (req "type" (string_enum ["horizontal", ()]))
+           (req "children" (list element_encoding))
+           (opt "id" (id_encoding container_encoding)))
+        (function Horizontal (children, id) -> Some ((), children, id) | _ -> None)
+        (fun ((), children, id) -> Horizontal (children, id)) ;
       case
-        (with_kind "component" component_encoding)
-        (function Component props -> Some props | _ -> None)
-        (fun props -> Component props) ]
+        (obj3
+           (req "type" (string_enum ["flow", ()]))
+           (req "children" (list element_encoding))
+           (opt "id" (id_encoding container_encoding)))
+        (function Flow (children, id) -> Some ((), children, id) | _ -> None)
+        (fun ((), children, id) -> Flow (children, id)) ;
+      case
+        (obj3
+           (req "type" (string_enum ["menu", ()]))
+           (req "children" (list element_encoding))
+           (opt "id" (id_encoding container_encoding)))
+        (function Menu (children, id) -> Some ((), children, id) | _ -> None)
+        (fun ((), children, id) -> Menu (children, id)) ;
+      case
+        (obj3
+           (req "type" (string_enum ["custom_container", ()]))
+           (req "children" (list element_encoding))
+           (req "id" (id_encoding container_encoding)))
+        (function Custom_container (children, id) -> Some ((), children, id) | _ -> None)
+        (fun ((), children, id) -> Custom_container (children, id)) ;
+      case
+        (obj3
+           (req "type" (string_enum ["label", ()]))
+           (dft "text" string "")
+           (opt "id" (id_encoding component_encoding)))
+        (function Label (text, id) -> Some ((), text, id) | _ -> None)
+        (fun ((), text, id) -> Label (text, id)) ;
+      case
+        (obj2
+           (req "type" (string_enum ["custom_component", ()]))
+           (req "id" (id_encoding component_encoding)))
+        (function Custom_component id -> Some ((), id) | _ -> None)
+        (fun ((), id) -> Custom_component id) ]
+
+let view_encoding =
+  conv
+    (fun { document ; compatible_profiles } ->
+       (document, compatible_profiles))
+    (fun (document, compatible_profiles) ->
+       { document ; compatible_profiles }) @@
+  obj2
+    (req "document" element_encoding)
+    (req "profiles" (list (id_encoding profile_encoding)))
+
+let page_encoding =
+  def "page" @@
+  conv
+    (fun { views ; components ; containers ; sources } ->
+       (views, components, containers, sources))
+    (fun (views, components, containers, sources) ->
+       { views ; components ; containers ; sources }) @@
+  obj4
+    (req "views" (list view_encoding))
+    (dft "components" (table_encoding component_encoding) [])
+    (dft "containers" (table_encoding container_encoding) [])
+    (dft "sources" (table_encoding source_encoding) [])
+
+let ui_encoding =
+  conv
+    (fun { pages ; profiles } -> (pages, profiles))
+    (fun (pages, profiles) -> { pages ; profiles }) @@
+  obj2
+    (req "pages" (table_encoding page_encoding))
+    (req "profiles" (table_encoding profile_encoding))
 
 let () =
-  Json_encoding.schema element_encoding |>
+  Json_encoding.schema ui_encoding |>
   Json_schema.to_json |>
-  Ezjsonm.to_channel ~minify:false stdout
-*)
+  Ezjsonm.to_channel ~minify:false stdout ;
+  Printf.fprintf stdout "\n%!" ;
+  let rec main () =
+    try
+      let json = Ezjsonm.from_channel stdin in
+      ignore (destruct ui_encoding json)
+    with err ->
+      let print_unknown ppf = function
+        | Ezjsonm.Parse_error (_, err) ->
+          Format.fprintf ppf "%s" err
+        | exn -> raise exn in
+      Format.eprintf "%a@." (print_error ~print_unknown) err ;
+      main () in
+  main ()
