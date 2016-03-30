d180d7164e0ef9482486e27b83a3ccd558cea20f
Benjamin Canou <benjamin@canou.fr>
2016-03-30 17:36:33 +0200

    Work on the checker.

--- rddl_checker.ml
+++ rddl_checker.ml
@@ -20,17 +20,24 @@
 open Rddl_ast
 open Rddl_profile
 
-exception Duplicate_profile_id of profile id
-exception Duplicate_page_id of page id
-exception Duplicate_component_id of page id * component id
-exception Duplicate_container_id of page id * container id
+exception Duplicate_profile of profile id
+exception Clashing_profiles of profile id * profile id * profile
+exception Duplicate_page of page id
+exception Duplicate_component of page id * component id
+exception Duplicate_container of page id * container id
+exception Missing_component of page id * int * component id
+exception Missing_container of page id * int * container id
+exception Unknown_component of page id * int * component id
+exception Unknown_container of page id * int * container id
+exception Component_appearing_twice of page id * int * component id
+exception Container_appearing_twice of page id * int * container id
 exception Profile_handled_by_two_views of page id * profile id * int * int
 exception Profile_not_handled of page id * profile id
-exception Clashing_profiles of profile id * profile id * profile
-exception Unkown_profile of page id * int * profile id
+exception Unknown_profile of page id * int * profile id
 
-let check ui =
-  let recoverables = ref [] in
+(* The generic checker, which may stop upon the first error or not,
+   depending on the [raise] callback. *)
+let check raise ui =
   let htable table error =
     let htable = Hashtbl.create 10 in
     List.iter
@@ -41,47 +48,84 @@ let check ui =
            Hashtbl.add htable id elt)
       table ;
     htable in
-  try
-    let profiles =
-      htable ui.profiles
-        (fun id -> Duplicate_profile_id id) in
-    Hashtbl.iter (fun id profile ->
-        Hashtbl.iter (fun id' profile' ->
-            if compatible profile profile' then
-              let overlap = meet profile profile' in
-              let exn = Clashing_profiles (id, id', overlap) in
-              recoverables := exn :: !recoverables)
-          profiles)
-      profiles ;
-    let pages =
-      htable ui.pages
-        (fun id -> Duplicate_page_id id) in
-    Hashtbl.iter (fun page_id page ->
-        let _components =
-          htable page.components
-            (fun id -> Duplicate_component_id (page_id, id)) in
-        let _containers =
-          htable page.containers
-            (fun id -> Duplicate_container_id (page_id, id)) in
-        let handled_profiles = Hashtbl.create 10 in
-        List.iteri
-          (fun i view ->
-             List.iter
-               (fun id ->
-                  begin try
-                      let prev = Hashtbl.find handled_profiles id in
-                      let exn = Profile_handled_by_two_views (page_id, id, prev, i) in
-                      recoverables := exn :: !recoverables
-                    with Not_found -> ()
-                  end ;
-                  Hashtbl.add handled_profiles id i)
-               view.compatible_profiles)
-          page.views ;
-        Hashtbl.iter (fun id _ ->
-            if not (Hashtbl.mem handled_profiles id) then
-              raise (Profile_not_handled (page_id, id)))
-          profiles)
-      pages ;
-    !recoverables
-  with
-    fatal -> fatal :: !recoverables
+  let profiles =
+    htable ui.profiles
+      (fun id -> Duplicate_profile id) in
+  Hashtbl.iter (fun id profile ->
+      Hashtbl.iter (fun id' profile' ->
+          if compatible profile profile' then
+            let overlap = meet profile profile' in
+            raise (Clashing_profiles (id, id', overlap)))
+        profiles)
+    profiles ;
+  let pages =
+    htable ui.pages
+      (fun id -> Duplicate_page id) in
+  Hashtbl.iter (fun page_id page ->
+      let components =
+        htable page.components
+          (fun id -> Duplicate_component (page_id, id)) in
+      let containers =
+        htable page.containers
+          (fun id -> Duplicate_container (page_id, id)) in
+      let handled_profiles = Hashtbl.create 10 in
+      List.iteri
+        (fun i view ->
+           let appearing_containers = Hashtbl.create 10 in
+           let appearing_components = Hashtbl.create 10 in
+           let rec traverse = function
+             | Container (_cstr, children, None) ->
+               List.iter traverse children
+             | Container (_cstr, children, Some id) ->
+               if not (Hashtbl.mem containers id) then
+                 raise (Unknown_container (page_id, i, id)) ;
+               if Hashtbl.mem appearing_containers id then
+                 raise (Container_appearing_twice (page_id, i, id)) ;
+               Hashtbl.add appearing_containers id () ;
+               List.iter traverse children
+             | Component (_cstr, id) ->
+               if not (Hashtbl.mem components id) then
+                 raise (Unknown_component (page_id, i, id)) ;
+               if Hashtbl.mem appearing_components id then
+                 raise (Component_appearing_twice (page_id, i, id)) ;
+               Hashtbl.add appearing_components id () in
+           traverse view.document ;
+           Hashtbl.iter
+             (fun id { container_priority = prio } -> match prio with
+                | Optional -> ()
+                | Required | Hideable ->
+                  if not (Hashtbl.mem appearing_containers id) then
+                    raise (Missing_container (page_id, i, id)))
+             containers ;
+           Hashtbl.iter
+             (fun id { component_priority = prio } -> match prio with
+                | Optional -> ()
+                | Required | Hideable ->
+                  if not (Hashtbl.mem appearing_components id) then
+                    raise (Missing_component (page_id, i, id)))
+             components ;
+           List.iter
+             (fun id ->
+                begin try
+                    let prev = Hashtbl.find handled_profiles id in
+                    raise (Profile_handled_by_two_views (page_id, id, prev, i))
+                  with Not_found -> ()
+                end ;
+                Hashtbl.add handled_profiles id i)
+             view.compatible_profiles)
+        page.views ;
+      Hashtbl.iter (fun id _ ->
+          if not (Hashtbl.mem handled_profiles id) then
+            raise (Profile_not_handled (page_id, id)))
+        profiles)
+    pages
+
+let wellformed ui =
+  check raise ui
+
+let check ui =
+  let recoverables = ref [] in
+  let dont_raise exn =
+    recoverables := exn :: !recoverables in
+  check dont_raise ui ;
+  !recoverables
--- rddl_checker.mli
+++ rddl_checker.mli
@@ -19,13 +19,51 @@
 
 open Rddl_ast
 
-exception Duplicate_profile_id of profile id
-exception Duplicate_page_id of page id
-exception Duplicate_component_id of page id * component id
-exception Duplicate_container_id of page id * container id
-exception Profile_handled_by_two_views of page id * profile id * int * int
-exception Profile_not_handled of page id * profile id
-exception Clashing_profiles of profile id * profile id * profile
-exception Unkown_profile of page id * int * profile id
+(** A profile ID is used twice. *)
+exception Duplicate_profile of profile id
 
+(** Two profiles overlap.
+    Gives the computed conflicting intersection. *)
+exception Clashing_profiles of profile id * profile id * profile
+
+(** A page ID is used twice. *)
+exception Duplicate_page of page id
+
+(** A component ID is used twice inside a page. *)
+exception Duplicate_component of page id * component id
+
+(** A container ID is used twice inside a page. *)
+exception Duplicate_container of page id * container id
+
+(** A required component is missing inside a view. *)
+exception Missing_component of page id * int * component id
+
+(** A required container is missing inside a view. *)
+exception Missing_container of page id * int * container id
+
+(** An undeclared component ID appears inside a view. *)
+exception Unknown_component of page id * int * component id
+
+(** An undeclared container ID appears inside a view. *)
+exception Unknown_container of page id * int * container id
+
+(** An component ID appears twice inside a view. *)
+exception Component_appearing_twice of page id * int * component id
+
+(** An container ID appears twice inside a view. *)
+exception Container_appearing_twice of page id * int * container id
+
+(** Two views are assigned the same profile ID . *)
+exception Profile_handled_by_two_views of page id * profile id * int * int
+
+(** No view is assigned some profile ID . *)
+exception Profile_not_handled of page id * profile id
+
+(** A view is assigned an unknown profile ID . *)
+exception Unknown_profile of page id * int * profile id
+
+(** Check an RDDL definition and return the full list of errors. *)
 val check : ui -> exn list
+
+(** Check an RDDL definition, stop and raise the first error. *)
+val wellformed : ui -> unit
